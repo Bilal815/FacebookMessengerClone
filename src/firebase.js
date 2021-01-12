408e08d671d971c50253105ef495bcd63cbf0fe6
Bilal Ahmed <62904483+Bilal815@users.noreply.github.com>
2021-01-12 14:15:11 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyCBmK-ZAHxXhaavfTL0WSv7mVwRvdn30gg",
-  authDomain: "facebook-messenger-clone.firebaseapp.com",
-  databaseURL: "https://facebook-messenger-clone.firebasio.com",
-  projectId: "facebook-messenger-clone",
-  storageBucket: "facebook-messenger-clone.appspot.com",
-  messagingSenderId: "226212015774",
-  appId: "1:226212015774",
-  measurementId: "G-FB4NJEW2FT"
+  apiKey: "AIzaSyCFP-npsjbViWgSa7JmOILSXsqB9CxfkJk",
+  authDomain: "facebook-messenger-clone-fa09d.firebaseapp.com",
+  databaseURL: "https://facebook-messenger-clone-fa09d-default-rtdb.firebaseio.com",
+  projectId: "facebook-messenger-clone-fa09d",
+  storageBucket: "facebook-messenger-clone-fa09d.appspot.com",
+  messagingSenderId: "1068710643229",
+  appId: "1:1068710643229:web:d3713d5946bc7a7cfda8f4",
+  measurementId: "G-G9DDJR7D2B"
 });
 
 const db = firebaseApp.firestore();
